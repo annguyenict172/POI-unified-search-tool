f4730dab5b3fa8a1355417ae05900114da0ecac3
An Nguyen <anthailan1702@gmail.com>
2019-04-06 11:32:29 +0700

    Implement simple explore endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from marshmallow import Schema, fields
+import requests
 
 from config import Config
-
+from app.helper import parse_args_with
+from app.constant import FoursquareEndpoint, GoogleEndpoint
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    return 'Hello world'
+class FindPlaceSchema(Schema):
+    query = fields.String(required=True)
+
+
+@app.route('/places/explore')
+@parse_args_with(FindPlaceSchema)
+def explore_places(args):
+    response = requests.get(GoogleEndpoint.FREE_TEXT_SEARCH, params={
+        'query': args['query'],
+        'key': Config.GOOGLE_PLACE_API_KEY
+    })
+    return jsonify(response.json())
