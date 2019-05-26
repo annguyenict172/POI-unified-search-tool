26babae03f3c43cad3657b1bd67033a3e858c766
Hiep <hoanghiepnguyen106@gmail.com>
2019-05-26 07:38:45 +0700

    Change logic to adapt with the new cache server

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 
 from config import Config
 from app.decorators import parse_args_with
+from libs.cache import retrieve_from_cache, store_to_cache
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -23,16 +24,39 @@ from app.data_preprocessor import DataPreprocessor
 class FindPlaceSchema(Schema):
     location = fields.String(required=True)
     keyword = fields.String(missing=None)
-    radius = fields.Integer(missing=None)
+    radius = fields.Float(missing=1)
     categories = fields.String(missing=None)
+    page = fields.Integer(missing=None)
 
 
 @app.route('/places/explore')
 @parse_args_with(FindPlaceSchema)
 def explore_places(args):
+    keyword = args.get('keyword')
+    if not keyword:
+        results, page, total_pages = retrieve_from_cache(args)
+        if results is not None:
+            return jsonify({
+                'results': results,
+                'page': page,
+                'total_pages': total_pages,
+            })
     arguments = parameter_parser.parse_parameters(raw_params=args)
     responses = APIDispatcher(args=arguments).dispatch_api_calls()
     preprocessed_data = DataPreprocessor(data=responses).process_data()
     results = DataAggregator(data=preprocessed_data).aggregate_data()
 
-    return jsonify(results)
+    if not keyword and len(results) > 0:
+        first_page_results, total_pages = store_to_cache(args, results)
+        if first_page_results is not None:
+            return jsonify({
+                'results': first_page_results,
+                'page': 1,
+                'total_pages': total_pages,
+            })
+
+    return jsonify({
+        'results': results,
+        'page': 1,
+        'total_pages': 1,
+    })
